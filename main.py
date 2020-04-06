0bb09926764aba97653fe8337abacc8ce762a661
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-06 15:17:32 -0500

    An iteration in the hello template was created and in the main file a dictionary of todo activities was createated.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -9,7 +12,12 @@ def index():
     response.set_cookie('user_ip', user_ip)
     return response
 
+
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return render_template('hello.html', user_ip = user_ip)
+    context = {
+        'user_ip' : user_ip, 
+        'todos' : todos,
+    }
+    return render_template('hello.html', **context )
