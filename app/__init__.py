a40ef9524f822c3848c7963d80a6d39324d5541f
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-06 17:23:47 -0500

    The new Blueprint for login was created.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from .config import Config
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
@@ -9,4 +10,5 @@ def create_app():
     app.config.from_object(Config)
 
     app.config['SECRET_KEY'] = 'SUPER SECRETO'
+    app.register_blueprint(auth)
     return app
