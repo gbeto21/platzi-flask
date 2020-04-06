7177e516fed9b6b1cacb42bee0038d0b23da8f71
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-06 15:03:55 -0500

    A new template was created.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
@@ -12,5 +12,4 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return 'Hello Wold Platzi, tu IP es: {}'.format(user_ip)
-    #return
+    return render_template('hello.html', user_ip = user_ip)
