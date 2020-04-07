9c4ce1a3791098d11670dc161f987be54409eb3e
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-06 19:00:07 -0500

    An access to the database was created.

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from wtforms.validators import DataRequired
 import unittest
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
 
 app = create_app()
-
-todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor.']
 
 
 @app.cli.command()
@@ -38,13 +37,17 @@ def index():
 @app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'user_ip' : user_ip, 
-        'todos' : todos,
+        'todos' : get_todos(user_id = username),
         'username': username
     }
- 
+
+    users = get_users()
+    for user in users:
+        print(user.id)
+        print(user.to_dict()['password'])
+
     return render_template('hello.html', **context )
