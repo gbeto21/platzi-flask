0a27ca633a69294e3b553a963af6f49e92233ba9
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-06 20:25:42 -0500

    The fountion to logup was created.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, session, redirect, flash, url_for
 from flask_login import login_user, login_required, logout_user
-
+from werkzeug.security import generate_password_hash
 from app.forms import LoginForm
 from . import auth
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.models import UserModel, UserData
 
 @auth.route('/login', methods=['GET','POST'])
@@ -36,6 +36,32 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html',**context)
+
+@auth.route('signup', methods=['GET', 'POST'])
+def sigunp():
+    sigunp_form = LoginForm()
+    context = {
+        'signup_form': sigunp_form
+    }
+
+    if sigunp_form.validate_on_submit():
+        username = sigunp_form.username.data
+        password = sigunp_form.password.data
+
+        user_doc = get_user(username)
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+            user = UserModel(user_data)
+            login_user(user)
+            flash('Bienvenido.')
+            return redirect(url_for('hello'))
+            
+        else:
+            flash('El usuario existe.')
+
+    return render_template('signup.html', **context)
 
 @auth.route('logout')
 @login_required
