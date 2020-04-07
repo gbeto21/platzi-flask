0bddd911c222a005add15a5aca79528a59ddf2f0
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-07 15:08:15 -0500

    The founction to delete a todo was created.

--- app/forms.py
+++ app/forms.py
@@ -10,4 +10,7 @@ class LoginForm(FlaskForm):
 class TodoForm(FlaskForm):
     description = StringField('Descripción', validators=[DataRequired()])
     submit = SubmitField('Crear')
+
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
     
