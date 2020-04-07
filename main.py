b190cdfe23ac7e8dd6fd3cf8a543b2c9e34c11fc
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-06 20:47:46 -0500

    The function to add a new todo was created.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired 
 from app import create_app
-from app.forms import LoginForm
-from app.firestore_service import get_users, get_todos
+from app.forms import TodoForm
+from app.firestore_service import get_users, get_todos, put_todo
 from flask_login import login_required, current_user
 
 app = create_app()
@@ -35,16 +35,23 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET','POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
+    todo_form = TodoForm()
 
     context = {
         'user_ip' : user_ip, 
         'todos' : get_todos(user_id = username),
-        'username': username
+        'username': username,
+        'todo_form': todo_form
     }
+
+    if todo_form.validate_on_submit():
+        put_todo(user_id = username, description=todo_form.description.data)
+        flash('Tu tarea se creo con éxito.')
+        return redirect(url_for('hello'))
 
     return render_template('hello.html', **context )
