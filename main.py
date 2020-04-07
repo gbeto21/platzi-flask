ef44dd38555cf9f023721236d1c321703efac908
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-07 15:39:58 -0500

    The update todo was created.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired 
 from app import create_app
-from app.forms import TodoForm, DeleteTodoForm
-from app.firestore_service import get_users, get_todos, put_todo, delete_todo
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+from app.firestore_service import update_todo, get_todos, put_todo, delete_todo
 from flask_login import login_required, current_user
 
 app = create_app()
@@ -42,6 +42,7 @@ def hello():
     username = current_user.id
     todo_form = TodoForm()
     delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
 
     context = {
         'user_ip' : user_ip, 
@@ -49,6 +50,7 @@ def hello():
         'username': username,
         'todo_form': todo_form,
         'delete_form': delete_form,
+        'update_form': update_form,
     }
 
     if todo_form.validate_on_submit():
@@ -64,4 +66,11 @@ def delete(todo_id):
     user_id = current_user.id
     delete_todo(user_id=user_id, todo_id=todo_id)
     
+    return redirect(url_for('hello'))
+
+
+@app.route('/todos/update/<todo_id>/<int:done>', methods=['POST'])
+def update(todo_id, done):
+    user_id = current_user.id
+    update_todo(user_id=user_id,todo_id=todo_id,done=done)
     return redirect(url_for('hello'))
